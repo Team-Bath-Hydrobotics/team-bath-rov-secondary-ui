4013c5f445bf1681733537309a7b765fdc616071
mbm60-max <mbm60@bath.ac.uk>
2026-01-01 18:30:26 +0000

    try fixing linting

--- eslint.config.js
+++ eslint.config.js
@@ -1,45 +1,44 @@
 import js from '@eslint/js'
 import globals from 'globals'
-import react from 'eslint-plugin-react'
+import reactPlugin from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
-import { defineConfig, globalIgnores } from 'eslint/config'
 
-export default defineConfig([
-  globalIgnores(['dist']),
+export default tseslint.config(
+  { ignores: ['dist'] },
   {
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      react.configs.flat.recommended,
-      reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
-    ],
     languageOptions: {
-      ecmaVersion: 'latest',
+      ecmaVersion: 2020,
       globals: globals.browser,
+    },
+    plugins: {
+      'react': reactPlugin,
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+      'react/react-in-jsx-scope': 'off',
+      'react/jsx-uses-react': 'off',
+      'no-restricted-syntax': [
+        'error',
+        {
+          selector: 'ClassDeclaration',
+          message: 'Class components are not allowed. Use function components with hooks.',
+        },
+      ],
     },
     settings: {
       react: {
         version: 'detect',
       },
     },
-    rules: {
-      // React 17+ JSX transform
-      'react/react-in-jsx-scope': 'off',
-      'react/jsx-uses-react': 'off',
-
-      // Enforce hook only components
-      'no-restricted-syntax': [
-        'error',
-        {
-          selector: 'ClassDeclaration',
-          message:
-            'Class components are not allowed. Use function components with hooks.',
-        },
-      ],
-    },
   },
-])
+)
